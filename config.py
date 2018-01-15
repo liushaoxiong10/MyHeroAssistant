8c9611f69c7d0f3d5c8f8dcf1b9a84d3a1e7473a
Koala <liushaoxiong10@outlook.com>
2018-01-15 14:35:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 """ （百度ocr）你的 APPID AK SK """
-APP_ID = '10689710'
-API_KEY = 'M1dw61pRlDAYqvp67M4wQPrk'
-SECRET_KEY = '6xvtyl5jmghHjmMTss9bE93d9WZpMlrj '
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ' '
